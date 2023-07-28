caa9b40b79238a95c2cf0f7453d8585ef746633f
Sanaboina Swetha <126000859+SanaboinaSwetha@users.noreply.github.com>
2023-07-28 20:16:48 +0530

    Add files via upload

---
+++ biography/design.js
@@ -0,0 +1,33 @@
+ReactDOM.render(<div align="center"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQg_KSbOMcujsiVTf5kBZ3NoZogHbjrqNXdg&usqp=CAU" height="300px" width="300px" />
+    <h1>Shakunthala Devi</h1>
+<h3>Shakuntala Devi Contribution to Maths</h3>
+<p>Shakuntala Devi- The Human-Computer of the world as she was called made a great contribution to maths. She was born on 4th Nov 1929 in a Brahmin family in Bangalore city of India. She had an extraordinary mathematical skill which has inspired millions of students across the world. At an early age she became world-famous through her superb arithmetic skills. She belonged to a lower-middle-class family and her father used to work in a circus. He had discovered extraordinary mathematical skills at an early age. Then he made her participate in the mathematical talent shows. She had to drop out of school since her family was not able to afford her fees. But she is a born-genius and was able to solve difficult maths calculations even without the need for formal education in school. Even at the age of six, she used to visit Universities to give public performances like the University of Mysore, Annamalai University, Osmania University to name a few, and brilliantly she used to answer all the questions. She was becoming famous over the period of time and started to earn bigger money even being a child.
+
+Seeing a bigger opportunity overseas she started touring across the world starting with London in the 1950s where she got popular in media with BBC giving her the title of Human-Computer. In her mathematical skills of Speed and Accuracy, she could beat the computers.</p>
+<h3>Her Achievements</h3>
+<h3>Feats of Shakuntala Devi and her contribution to maths</h3>
+<p>Some of the Feats of Shakuntala Devi include:-
+In Dallas at Sothern Methodist College, she calculated the 23rd cube root of 201 digit number in 50 seconds giving the answer 546,372,891 in comparison to a UNIVAC 1101 computer which took more time to around 12 more seconds to do the same calculation.
+At Imperial College London, 1980 she multiplied 7686369774870 and 2465099745779 calculating the 26 digit answer in 28 seconds. She holds Guinness Book of World Record for this fastest human computation.
+At Stanford University US, 1988,
+She calculated cube root of 95443993 as 457 in 2 seconds
+She calculated cube root of 2373927704 as 1334 in 10 seconds
+She calculated the 8th root of 20047612231936 as 46 in 10 seconds
+As per New York Times “She could give you the cube root of 188,132,517 — or almost any other number — in the time it took to ask the question. If you gave her any date in the last century, she would tell you what day of the week it fell on”</p>
+<h3>Writings/ Selected Works of Shakuntala Devi</h3>
+<p>Mathability: Awaken the Math Genius in Your Child
+More Puzzles to Puzzle
+Astrology for You
+Book of Numbers 
+Figuring: The Joy of Numbers
+In the Wonderland of Numbers
+Perfect Murder
+Puzzles to Puzzle You
+Super Memory: It Can Be Yours</p>
+<br/>
+<h3>Was Shakuntla Devi an astrologer?</h3>
+<p>Shakuntala Devi, a great mathematician also started astrology. By studying stars, planets of an individual as per the date of birth, she used to make predictions about personal lives, describe their personalities, and offered advice. She has also written a book on astrology- Astrology for you. The book gives information on planets, zodiacs etc</p>
+<h3>Death of Shakuntala Devi</h3>
+<p>Shakuntla Devi died at the age of 83 on 21 April 2013 in Bangalore. She had respiratory problems. With her extraordinary skills and geniuses she inspired math learners across the world. A biopic movie has also been made where the role of Shakuntala Devi has been played by Vidya Balan.</p>
+</div>,document.getElementById("content"));
+
